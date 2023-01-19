548bde8480cf0ad29911f8dde6472a81e8551156
devgony <devgony@gmail.com>
2023-01-19 16:52:07 +0900

    build: init yew + tailwindcss

---
+++ src/app.rs
@@ -0,0 +1,11 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <main>
+          <div class="bg-gray-300">{"NavBar"}</div>
+          <div class="bg-gray-">{"NavBar"}</div >
+        </main>
+    }
+}
